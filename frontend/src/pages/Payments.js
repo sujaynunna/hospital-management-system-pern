9a63ddd50b9cd55e60767b5605da37a2b4917fa0
sujaynunna <sujaynunna21@gmail.com>
2026-03-11 08:09:12 +0530

    Update Payments.js

--- frontend/src/pages/Payments.js
+++ frontend/src/pages/Payments.js
@@ -1,15 +1,36 @@
+
+ import { useState } from "react";
 import { makePayment } from "../api/api";
 
-function Payments({ billId, refresh }) {
+function Payments({ billId, remaining, refresh }) {
+  const [amount, setAmount] = useState("");
+
   const handlePayment = async () => {
-    await makePayment({ billId });
+    if (!amount) return alert("Enter amount");
+
+    await makePayment({
+      billId,
+      amount
+    });
+
+    setAmount("");
     refresh();
   };
 
   return (
-    <button onClick={handlePayment} className="btn btn-success">
-      Pay Now
-    </button>
+    <div className="d-flex gap-2">
+      <input
+        type="number"
+        className="form-control"
+        placeholder={`Max ₹${remaining}`}
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+
+      <button onClick={handlePayment} className="btn btn-success">
+        Pay
+      </button>
+    </div>
   );
 }
 
